7198c600a94369c6485321482930f0eecfa07640
Abenezer <130685937+Aben854@users.noreply.github.com>
2025-04-20 12:45:33 -0400

    Update Convertor with GUI.py

--- Convertor with GUI.py
+++ Convertor with GUI.py
@@ -1,19 +1,22 @@
+# imports functions from customtkinter library and requests library to get currency exchange rates
 from customtkinter import *
 import requests
 
+# uses Frankfurter api to fetch current exchange rates in order to convert base currency (from_curr) to a different currency (to_curr)
 def convert_currency(amount, from_curr, to_curr):
     try:
         
         url = f"https://api.frankfurter.app/latest?amount={amount}&from={from_curr}&to={to_curr}"
         response = requests.get(url)
+
+# raises an error status if the api fails 
         response.raise_for_status()
         result = response.json()
         return round(result['rates'][to_curr], 2)
     except Exception as e:
         print("API failed", e)
-        rate = CURRENCY_CONVERSION.get(to_curr, 1) / CURRENCY_CONVERSION.get(from_curr, 1)
-        return round(amount * rate, 2)
     
+# converts currency and shows result visually
 def on_convert():
     try:
         amount = float(amount_entry.get())
@@ -24,15 +27,18 @@ def on_convert():
     except ValueError:
         result_label.configure(text="Please enter a valid number.")
       
+# GUI Setup using customtkinter
 app = CTk()
 app.geometry("800x500")
 app.title("GlobeChange")
 set_appearance_mode("Dark")
 set_default_color_theme("green")
 
+# Amount input text box
 amount_entry = CTkEntry(app, placeholder_text="Enter amount")
 amount_entry.pack(pady=5)
 
+# Different currencies drop down selection
 from_currency = CTkComboBox(app, values=["USD", "EUR", "GBP", "JPY", "KWD"])
 to_currency = CTkComboBox(app, values=["USD", "EUR", "GBP", "JPY", "KWD"])
 from_currency.pack(pady=5)
@@ -40,12 +46,15 @@ to_currency.pack(pady=5)
 from_currency.set("USD")
 to_currency.set("EUR")
 
+# Label on GUI
 label = CTkLabel(app, text="Currency Converter", font=("Arial", 24))
 label.pack(pady=10)
 
+# News Button
 btn = CTkButton(master=app, text="News")
 btn.place(relx=0.2, rely=0.05, anchor="ne")
 
+# Convert Currency Button 
 convert_btn = CTkButton(master=app, text="Convert", command=on_convert)
 convert_btn.pack(pady=10)
 
